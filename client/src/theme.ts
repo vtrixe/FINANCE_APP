933e8df71179a24ced81db42353bc41c09c8c7bc
VIKRANT SHARMA <vikrant500sharma@gmail.com>
2023-04-09 12:16:55 +0530

    commit

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -69,30 +69,30 @@ export const themeSettings = {
     },
   },
   typography: {
-    fontFamily: ["Inter", "sans-serif"].join(","),
+    fontFamily: ["Montserrat", "sans-serif"].join(","),
     fontSize: 12,
     h1: {
-      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontFamily: ["Montserrat", "sans-serif"].join(","),
       fontSize: 32,
     },
     h2: {
-      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontFamily: ["Montserrat", "sans-serif"].join(","),
       fontSize: 24,
     },
     h3: {
-      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontFamily: ["Montserrat", "sans-serif"].join(","),
       fontSize: 20,
       fontWeight: 800,
       color: tokens.grey[200],
     },
     h4: {
-      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontFamily: ["Montserrat", "sans-serif"].join(","),
       fontSize: 14,
       fontWeight: 600,
       color: tokens.grey[300],
     },
     h5: {
-      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontFamily: ["Montserrat", "sans-serif"].join(","),
       fontSize: 12,
       fontWeight: 400,
       color: tokens.grey[500],
